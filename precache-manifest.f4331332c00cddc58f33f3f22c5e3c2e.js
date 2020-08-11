e7162e8e412075ad193a8a22ed648020031b0991
myhkas <mmyhkas@gmail.com>
2020-08-10 19:30:10 -0700

    Updates

---
+++ precache-manifest.f4331332c00cddc58f33f3f22c5e3c2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd786137aee5f34fd741ad6185041fdd",
+    "url": "/Aug10_demo/index.html"
+  },
+  {
+    "revision": "b6bf01283728ac0c11b1",
+    "url": "/Aug10_demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/Aug10_demo/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aug10_demo/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6bf01283728ac0c11b1",
+    "url": "/Aug10_demo/static/js/main.5d0b4e10.chunk.js"
+  },
+  {
+    "revision": "278f6087451730d05eb5",
+    "url": "/Aug10_demo/static/js/runtime-main.ed10c03f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Aug10_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
